caa1e286092852ea0576986f8396703486ebc6df
abishkam <mollaev6@mail.ru>
2023-08-29 18:46:29 +0300

    init db

--- football-data/football-data-service/src/main/resources/db/migration/common/V01_00_00_001__init_schema.sql
+++ football-data/football-data-service/src/main/resources/db/migration/common/V01_00_00_001__init_schema.sql
@@ -1,66 +1,83 @@
-create table if not exists football.position(
-    id smallint not null
-        primary key,
-    name text not null unique
+create type football.position_type as enum(
+    'GK',
+    'CB',
+    'LB',
+    'RB',
+    'LWB',
+    'RWB',
+    'CDM',
+    'CM',
+    'LM',
+    'RM',
+    'CAM',
+    'ST',
+    'CF',
+    'LW',
+    'RW'
 );
 
 create table if not exists football.national_team(
-    id varchar(255) not null
+    id uuid not null
         primary key,
     name text not null unique,
     coach text
 );
 
 create table if not exists football.club(
-    id varchar(255) not null
+    id uuid not null
         primary key,
-    name text not null,
+    name text not null unique,
     league varchar(255) not null,
     stadium varchar(50)
 );
 
 create table if not exists football.championship(
-    id varchar(255) not null
-    primary key,
+    id uuid not null
+        primary key,
     name text not null unique
 );
 
 create table if not exists football.player(
-    id varchar(255) not null
-            primary key,
+    id uuid not null
+        primary key,
     name text not null,
-    ages smallint not null,
+    ages date not null,
     citizenship varchar(3) not null,
-    position smallint not null
-        constraint fk_position_position_id
-            references football.position,
-    national_team varchar(255)
-        constraint fk_national_team_national_team_id
+    player_position football.position_type not null,
+    national_team_id uuid
+        constraint fk_player_national_team_id
             references football.national_team,
-    club varchar(255)
-        constraint fk_club_club_id
+    club_id uuid
+        constraint fk_player_club_id
             references football.club
 );
 
 create table if not exists football.cup(
-    id varchar(255) not null
+    id uuid not null
         primary key,
-    name varchar(20) not null,
+    name text not null unique,
     year date not null,
-    team varchar(255) not null,
-    constraint fk_club_club_id foreign key (team) references football.club (id),
-    constraint fk_national_team_national_team_id foreign key (team) references football.national_team (id)
+    club_id uuid
+        constraint fk_cup_club_id
+            references football.club,
+    national_team_id uuid
+        constraint fk_cup_national_team_id
+            references football.national_team
 );
-
 alter table football.cup
-add constraint uq_name_year unique(name, year, team);
+    add constraint uq_name_year unique(name, year, club_id, national_team_id);
 
-
+-- Поменял many_to_many на many_to_one
 create table if not exists football.championship_team(
-    team_id varchar(255),
-    championship_id varchar(255),
-    primary key (team_id, championship_id),
-    foreign key(team_id) references football.club(id),
-    foreign key(team_id) references football.national_team(id),
-    foreign key(championship_id) references football.championship(id)
+    id uuid not null
+        primary key,
+    club_id uuid
+        constraint fk_championship_team_club_id
+            references football.club,
+    national_team_id uuid
+        constraint fk_championship_team_nt_id
+            references football.national_team,
+    championship_id uuid
+        constraint fk_championship_team_championship_id
+            references football.championship
 );
